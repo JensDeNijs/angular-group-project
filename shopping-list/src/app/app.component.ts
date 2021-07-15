2435c1918ab946dbd30f6de878ac34320a75d6ad
JensDeNijs <jensdenijs@hotmail.com>
2021-07-15 12:19:51 +0200

    added from + validation

--- shopping-list/src/app/app.component.ts
+++ shopping-list/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ListItem} from "./list-item";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopping-list';
+  public data = ["deep-frozen", "dairy", "drinks", "vegetables", "fruits", "snacks", "household-items", "others"];
+  public listItem = new ListItem("", "", "");
+  public regx = "^[A-Za-z0-9 ]+$";
+
 }
