024dde44cb1178c2ed16236b13f5c5c2bc9bba77
JensDeNijs <jensdenijs@hotmail.com>
2021-07-15 15:17:55 +0200

    add service

---
+++ shopping-list/src/app/add-item.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ListItem} from "./list-item";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddItemService {
+  private http: HttpClient;
+  public url: string = "http://localhost:4269/allItems"
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  addItem(item: ListItem) {
+    return this.http.post(this.url, item)
+  }
+
+}
